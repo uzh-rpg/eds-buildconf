35229d46cf926310ca9f25b3df5ec611fa085118
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2010-07-23 11:19:48 +0200

    make the bootstrap script more flexible and shell-compatible

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,20 @@
 #! /bin/sh
 
-ruby autoproj_bootstrap git http://gitorious.org/orocos-toolchain/build.git
+if ! test -f $PWD/autoproj_bootstrap; then
+    if which wget > /dev/null; then
+        DOWNLOADER=wget
+    elif which curl > /dev/null; then
+        DOWNLOADER=curl
+    else
+        echo "I can find neither curl nor wget, either install one of these or"
+        echo "download the following script yourself, and re-run this script"
+        exit 1
+    fi
+    $DOWNLOADER http://doudou.github.com/autoproj/autoproj_bootstrap
+fi
+
+ruby autoproj_bootstrap git git://gitorious.org/orocos-toolchain/build.git
+. $PWD/env.sh
 autoproj update
 autoproj fast-build
 
