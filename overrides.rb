0aab718c74d85f83f25401bd15db9ee38df190e3
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2010-08-11 12:15:13 +0200

    add commented-out stanza that reenabled the RTT test suite

--- overrides.rb
+++ overrides.rb
@@ -1,2 +1,8 @@
 # Write in this file customization code that will get executed after all the
 # soures have beenloaded.
+
+# Uncomment to reenable building the RTT test suite
+# This is disabled by default as it requires a lot of time and memory
+#
+# Autobuild::Package['rtt'].define "BUILD_TESTING", "ON"
+
