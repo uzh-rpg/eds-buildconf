a0dbbd9f179056790ed7216107af70b59c9da2ca
Peter Soetens <peter@thesourceworks.com>
2010-09-07 10:22:31 +0200

    bootstrap: checkout branch toolchain-2.0
    Signed-off-by: Peter Soetens <peter@thesourceworks.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,7 +13,7 @@ if ! test -f $PWD/autoproj_bootstrap; then
     $DOWNLOADER http://doudou.github.com/autoproj/autoproj_bootstrap
 fi
 
-ruby autoproj_bootstrap $@ git git://gitorious.org/orocos-toolchain/build.git
+ruby autoproj_bootstrap $@ git git://gitorious.org/orocos-toolchain/build.git branch=toolchain-2.0
 . $PWD/env.sh
 autoproj update
 autoproj fast-build
