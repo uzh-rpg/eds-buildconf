c0e42eccd4f2b88c880d29bce1a51334862717c7
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2010-11-11 15:55:32 +0100

    adapt the bootstrap.sh for rise/build-all

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,7 +13,7 @@ if ! test -f $PWD/autoproj_bootstrap; then
     $DOWNLOADER http://doudou.github.com/autoproj/autoproj_bootstrap
 fi
 
-ruby autoproj_bootstrap $@ git git://gitorious.org/orocos-toolchain/build.git branch=master
+ruby autoproj_bootstrap $@ git git://gitorious.org/rise/build-all.git branch=master
 . $PWD/env.sh
 autoproj update
 autoproj fast-build
