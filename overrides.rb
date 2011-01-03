19d4640d03067179dad39a4c0098ade422c1bc3a
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-01-03 14:34:11 +0100

    use a configuration option to enable/disable OCL compatibility
    The issue is that it requires quite a bit of tuning, so better
    ask the user and set the right configuration values where it
    is needed.

--- overrides.rb
+++ overrides.rb
@@ -11,7 +11,3 @@
 #
 # See config.yml to set the prefix:/opt/autoproj/2.0 globally for all packages.
 
-if package_selected?('rtt')
-    package('rtt').define "PLUGINS_ENABLE_SCRIPTING", "OFF"
-end
-
