813bdd8479e5ff535e40b18fedf899d7e47d48fb
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-02-08 11:52:57 +0100

    improve bootstrap.sh
      - we now want ruby 1.9. Make it an error to bootstrap under 1.8
      - try very hard to find a ruby 1.9 interpreter
      - make using the git protocol the default (i.e. what one gets if enter is
        pressed)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,29 @@
 #! /bin/sh
 
 CONF_REPO=rock/buildconf.git
-RUBY=ruby1.9.1
+RUBY=ruby
 
 set -e
 
 if ! which $RUBY > /dev/null 2>&1; then
-    echo "cannot find the ruby executable '$RUBY'"
-    echo "on ubuntu, you should run"
-    echo "  sudo apt-get install $RUBY rubygems"
+    echo "cannot find the ruby executable. On ubuntu, you should run"
+    echo "  sudo apt-get install ruby1.9.1 rubygems"
     exit 1
+fi
+if $RUBY --version | grep -q "1\.8"; then
+    if which ruby1.9.1 > /dev/null 2>&1; then
+        echo "ruby points to ruby 1.8. I am forcefully selecting ruby1.9.1 as the ruby executable"
+        echo "press ENTER to continue"
+        read LINE
+        RUBY=ruby1.9.1
+    else
+        echo "you are trying to bootstrap using Ruby 1.8. This is unsupported"
+        echo "please install Ruby 1.9.2 or later. On Debian and Ubuntu, it is done"
+        echo "with"
+        echo "  sudo apt-get install ruby1.9.1 rubygems"
+        echo "I am aborting now"
+        exit 1
+    fi
 fi
 
 if ! test -f $PWD/autoproj_bootstrap; then
@@ -25,22 +39,22 @@ if ! test -f $PWD/autoproj_bootstrap; then
     $DOWNLOADER http://rock-robotics.org/stable/autoproj_bootstrap
 fi
 
-echo "Do you want to use the git protocol to access the build configuration?"
-echo "If the protocol is blocked by your network answer with no."
+echo "Do you want to use the git protocol to access the build configuration ?"
+echo "If the protocol is blocked by your network answer with no. The default is yes."
 
 # Check and interprete answer of "[y|n]"
-ANSWER=""
-until [ "$ANSWER" = "y" ] || [ "$ANSWER" = "n" ] 
+ANSWER="wrong"
+until test "$ANSWER" = "y" || test "$ANSWER" = "n" || test "$ANSWER" = ""
 do
-	echo "Use git protocol? [y|n]"
-	read ANSWER
-	ANSWER=`echo $ANSWER | tr "[:upper:]" "[:lower:]"`
+    echo "Use git protocol? [y|n] (default: y)"
+    read ANSWER
+    ANSWER=`echo $ANSWER | tr "[:upper:]" "[:lower:]"`
 done
 
-if [ "$ANSWER" = "y" ]; then
-    $RUBY autoproj_bootstrap $@ git git://gitorious.org/$CONF_REPO push_to=git@gitorious.org:$CONF_REPO branch=master
-else
+if [ "$ANSWER" = "n" ]; then
     $RUBY autoproj_bootstrap $@ git http://git.gitorious.org/$CONF_REPO push_to=git@gitorious.org:$CONF_REPO branch=master
+else
+    $RUBY autoproj_bootstrap $@ git git://gitorious.org/$CONF_REPO push_to=git@gitorious.org:$CONF_REPO branch=master
 fi
 
 if test "x$@" != "xlocaldev"; then
