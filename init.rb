29f390856e887d346cc3064da85b4e1bcc2e1d47
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2014-05-25 19:23:59 +0200

    setup github access

--- init.rb
+++ init.rb
@@ -25,6 +25,7 @@
 
 require 'autoproj/gitorious'
 Autoproj.gitorious_server_configuration('GITORIOUS', 'gitorious.org')
+Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com')
 
 Autoproj.env_inherit 'CMAKE_PREFIX_PATH'
 
